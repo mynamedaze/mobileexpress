dd9602b8ee22a59d5eb7e8e76676edfdfc3e213c
Murat Kudainetov <murat.kudainetov@gmail.com>
2017-07-14 22:52:00 +0300

    собран фронтенд, подключены шрифты, размечена структура проекта. Сверстана сетка проекта, объявлены переменные, миксины, скаффолдинг. Подключен jQuery и Owl Corousel. Сверстан чистовой вариант шапки в раскраске. Сверстан черновой вариант блока intro.

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  $(".intro__utp-list").owlCarousel({
+    responsive:{
+        0:{
+            loop: true,
+            autoplay: true,
+            autoplayTimeout: 3000,
+            items:1
+        },
+        768:{
+            items:3,
+            touchDrag: false,
+            mouseDrag: false
+        },
+        1362:{
+            items:3,
+            touchDrag: false,
+            mouseDrag: false
+        }
+    }
+  });
+});
