2a8a36417bf17a19cc429a616ff7afeb147e1d0d
Murat Kudainetov <murat.kudainetov@gmail.com>
2017-07-19 01:47:59 +0300

    добавлен чистовой вариант с раскраской intro и mobile-car. Добавлен черновой вариант device-model и credit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/*оживляем карусель в интро*/
 $(document).ready(function(){
   $(".intro__utp-list").owlCarousel({
     responsive:{
@@ -20,3 +21,94 @@ $(document).ready(function(){
     }
   });
 });
+/*  */
+/*кнопка интро*/
+var telephoneField = document.getElementById('telephone-field');
+
+var introCallButton = document.getElementsByClassName('intro__call-button');
+
+var introFormField = document.getElementsByClassName('intro__form-field');
+
+$(introCallButton).click(function() {
+    $(introFormField).removeClass('intro__form-field--mobile');
+    $(introCallButton).addClass('disable');
+});
+/*  */
+
+/*прикручиваем валидатор для телефонов */
+$(document).ready(function(){
+  $(telephoneField).inputmask("+7 (999) 999-9999"); //specifying options
+});
+/*переключение тогглов интро*/
+var introTogglePhone = document.getElementsByClassName('device-model__button--iphone');
+var introTogglePad = document.getElementsByClassName('device-model__button--ipad');
+
+var modelListPhone = document.getElementsByClassName('device-model__model-list--phone');
+var modelListPad = document.getElementsByClassName('device-model__model-list--pad');
+
+var breakMainListPhone = document.getElementsByClassName('device-model__break-mainlist--phone');
+var breakMainListPad = document.getElementsByClassName('device-model__break-mainlist--pad');
+
+$(introTogglePhone).click(function() {
+    $(introTogglePhone).addClass('device-model__button--active');
+    $(introTogglePad).removeClass('device-model__button--active');
+
+    $(modelListPhone).removeClass('disable');
+    $(modelListPad).addClass('disable');
+
+    $(breakMainListPhone).removeClass('disable');
+    $(breakMainListPad).addClass('disable');
+});
+$(introTogglePad).click(function() {
+    $(introTogglePhone).removeClass('device-model__button--active');
+    $(introTogglePad).addClass('device-model__button--active');
+
+    $(modelListPhone).addClass('disable');
+    $(modelListPad).removeClass('disable');
+
+    $(breakMainListPhone).addClass('disable');
+    $(breakMainListPad).removeClass('disable');
+});
+
+/*переключение между мабасиками*/
+var currentItemPhone = 0;
+var modelItemsPhone = document.getElementsByClassName('device-model__model-item--phone');
+modelItemsPhone = Array.prototype.slice.call(modelItemsPhone);
+
+var currentBkeakListPhone = 0;
+var breakListPhone = document.getElementsByClassName('device-model__break-list--phone');
+breakListPhone = Array.prototype.slice.call(breakListPhone);
+
+modelItemsPhone.forEach(function (element, i) {
+    element.addEventListener('click', function () {
+        $(breakListPhone[currentBkeakListPhone]).addClass('disable');
+        $(breakListPhone[i]).removeClass('disable');
+        currentBkeakListPhone = i;
+
+        $(modelItemsPhone[currentItemPhone]).removeClass('device-model__model-item--active');
+        $(modelItemsPhone[i]).addClass('device-model__model-item--active');
+        currentItemPhone = i;
+    })
+});
+
+/*переключение между таблетки*/
+var currentItemPad = 0;
+var modelItemsPad = document.getElementsByClassName('device-model__model-item--pad');
+modelItemsPad = Array.prototype.slice.call(modelItemsPad);
+
+var currentBkeakListPad = 0;
+var breakListPad = document.getElementsByClassName('device-model__break-list--pad');
+breakListPad = Array.prototype.slice.call(breakListPad);
+
+modelItemsPad.forEach(function (element, i) {
+    element.addEventListener('click', function () {
+        $(breakListPad[currentBkeakListPad]).addClass('disable');
+        $(breakListPad[i]).removeClass('disable');
+        currentBkeakListPad = i;
+
+        $(modelItemsPad[currentItemPad]).removeClass('device-model__model-item--active');
+        $(modelItemsPad[i]).addClass('device-model__model-item--active');
+        currentItemPad = i;
+    })
+});
+  /* */
